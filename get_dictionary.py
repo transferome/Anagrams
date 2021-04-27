1cdf1be8dd0dd00ced7425be38cf7376a6c3df26
transferome <transferome1113@gmail.com>
2021-04-27 17:22:24 -0400

    read dictionary from url

---
+++ get_dictionary.py
@@ -0,0 +1,12 @@
+# biggest confusion with package to use is that people were using urllib, urllib2, or urllib3
+# urllib2 is not in python3, and urllib3 is not standard out of the box python module
+import urllib.request
+
+# opens url
+fp = urllib.request.urlopen("https://inventwithpython.com/dictionary.txt")
+# reads in the lines to a list, but they are not in 'UTF-8', they are in bytearray object type
+# which means it is unreadable to use in that form
+mbytes = fp.readlines()
+# this converts the bytearray into a 'utf8' string
+dictionary = [m.decode('utf8').rstrip('\n') for m in mbytes]
+fp.close()
